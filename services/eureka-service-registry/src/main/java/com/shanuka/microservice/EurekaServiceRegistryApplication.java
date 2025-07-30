d446d9be8d0cd0baf6ac93aa81446aa7ab6753b1
shanuka chathuranga <shanukachathuranga21@gmail.com>
2025-07-31 03:40:56 +0530

    eureka server implemented

--- services/eureka-service-registry/src/main/java/com/shanuka/microservice/EurekaServiceRegistryApplication.java
+++ services/eureka-service-registry/src/main/java/com/shanuka/microservice/EurekaServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package com.shanuka.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServiceRegistryApplication {
 
 	public static void main(String[] args) {
